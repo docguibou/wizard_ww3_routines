c30f3c4efe164e1d298b40e98d5aa7769fb1df88
Guillaume BOUTIN <gboutin@ifremer.fr>
2021-06-24 11:43:51 +0000

    sorting a bit more (case by case)

---
+++ input/Beaufort_case/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm  cpl_for*sh.o*  job_*sh.o*  nout.000000  debug.*   I_*.nc  WW3_*.nc grids.nc areas.nc masks.nc NANUK*.o* fob_*.o* 12s_*.o*  BEAUFORT* out_*ww3
+rm -r nextsim_outputs 
